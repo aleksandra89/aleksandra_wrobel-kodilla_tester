36c698180676e3dba31fe847d3459e16a09c6225
Aleksadra Wrobel <a.matysiak89@gmail.com>
2020-11-12 20:52:26 +0100

    zadanie 2.3

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,28 +1,34 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook("600g", 1000); //typ zmiennej jest taki jak nazwa klasy - klasa to foremka a obiekt to ciastko
+        Notebook notebook = new Notebook(600, 1000, 2000); //typ zmiennej jest taki jak nazwa klasy - klasa to foremka a obiekt to ciastko
         //System.out.println(notebook.weight);// obiekt to notebook, jesli chcemy wywolac sama zmienna to robimy.nazawazmiennej
         //System.out.println(notebook.price);
-        System.out.println(notebook.weight + " cena" + notebook.price);
-        //if(notebook.price<900) {
+        System.out.println(notebook.weight + " cena " + notebook.price + " rok " + notebook.year);
+        //if(no`tebook.price<900) {
            // System.out.println("This notebook is cheap");
        //}
         //else {
             //System.out.println("This notebook is quite expensive");
        // }
         notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYear();
 
-        Notebook heavyNotebook = new Notebook("2000g", 2000);
+        Notebook heavyNotebook = new Notebook(2000, 2000, 2003);
         //System.out.println(heavyNotebook.weight);
         //System.out.println(heavyNotebook.price);
-        System.out.println(heavyNotebook.weight + " cena" + heavyNotebook.price);
+        System.out.println(heavyNotebook.weight + " cena " + heavyNotebook.price + " rok " + heavyNotebook.year);
         heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkYear();
 
-        Notebook oldNotebook = new Notebook("1200g", 200);
+        Notebook oldNotebook = new Notebook(1200, 200, 1990);
         //System.out.println(oldNotebook.weight);
         //System.out.println(oldNotebook.price);
-        System.out.println(oldNotebook.weight + " cena" + oldNotebook.price);
+        System.out.println(oldNotebook.weight + " cena " + oldNotebook.price + " rok " + oldNotebook.year);
         oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkYear();
 
         //konkatenacja = laczenie argumentow ciagiem w jednym system.out
 
--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -1,19 +1,47 @@
 public class Notebook {
-        String weight;
+        int weight;
         int price;
+        int year;
 
-        public Notebook(String weight, int price) {
+
+        public Notebook(int weight, int price, int year) {  //konstrukor
                 this.weight = weight;
                 this.price = price;
+                this.year = year;
 
         }
 
-        public void checkPrice() {
-                if (this.price < 900) {
-                        System.out.println("This notebook is cheap");
+        public void checkPrice() {   //metoda
+
+                if (this.price < 600) {
+                        System.out.println("This notebook is  very cheap");
+                } else if (this.price >=600 && this.price <=1000) {
+                        System.out.println("This price is good");
                 } else {
-                        System.out.println("This notebook is expensive");
+                        System.out.println("This notebbok is expensive");
                 }
 
+        }
+        public void checkWeight() {
+
+                if (this.weight <= 600) {
+                        System.out.println("This notebook is to light");
+                } else if (this.weight > 600 && this.weight <= 1500) {
+                        System.out.println("This notebook is not so heavy");
+                } else {
+                        System.out.println("This notebook is to heavy");
+                }
+
+        }
+        public void checkYear() {
+                if (this.year <=1990 && this.price <2000) {
+                        System.out.println("This notebook is old and cheap");
+                }else if (this.year >=2000 && this.price < 2000) {
+                        System.out.println("This notebook is quite new and cheap");
+                }else if (this.year > 2000 && this.price >=2000) {
+                        System.out.println("This notebook is new and cheap");
+                } else {
+                        System.out.println("This is good notebook");
+                }
         }
 }
