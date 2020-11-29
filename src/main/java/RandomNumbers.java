2e190c7aead759938c13e78c5a86c541ae6e9c5f
Aleksadra Wrobel <a.matysiak89@gmail.com>
2020-11-29 14:33:31 +0100

    zadanie 2.5

--- src/main/java/RandomNumbers.java
+++ src/main/java/RandomNumbers.java
@@ -1,28 +1,16 @@
-/*
+import java.util.Random;
+
 public class RandomNumbers {
-
-    public static void main(String[] args){
-        int max = 5000;
-
-        public int randomNumber (int max){
-
-            Random random = new Random();
-            int result = 0;
-            int suma = 0;
-            while (suma <max) {
-
-                int i = random.nextInt(30);
-                suma = suma+ i;
-                result++;
-            }
-
-            return result;
-            }
-
-        }
-
+    public static void main(String[] args) {
+        int largest = 0;
+        int smallest = 0;
+        Random random = new Random();
+        int a = random.nextInt(31);
+        int sum = 0;
+        while (sum < 5000)
+            sum = sum + a;
+        System.out.println(sum);
 
 
     }
 }
-*/
