7dca7730d0334d251aed878e4d38b1f87f6c1d90
Aleksadra Wrobel <a.matysiak89@gmail.com>
2020-12-01 17:51:40 +0100

    zadanie 2.7

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,5 +1,15 @@
 public class Book {
+
     private String author;
     private String title;
+
+    private Book(String title, String author) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public static Book of(String author, String title) {
+        return new Book(author, title);
+    }
 
 }
